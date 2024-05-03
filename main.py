ae6686117db359244ef122e9639d8c3f483c5c37
Tyson Sakamoto <tysonsakamoto@gmail.com>
2024-05-03 02:38:38 -0700

    inital setup, not sure if this is how we want to do it

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-//hello
+#our classes
+#tree
+class Tree:
+    def __init__(self, root):
+        self.root = root
+    
+
+#node
+class Node:
+    def __init__(self, data):
+        self.data = data
+
+#problem
+class Problem:
+    pass
+
+
+#uniform cost search
+def ucs():
+    pass
+
+#A* with missing tile heuristic
+def A_star_tile():
+    pass
+
+
+#A* with Euclidean distance heuristic
+def A_star_euc():
+    pass
+
+
+#Main
+def main():
+    print("whats up")
+
+main()
