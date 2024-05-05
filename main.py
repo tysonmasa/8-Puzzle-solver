388ed43bc29735cab3a6c82f1f13f475c193c092
aguin005 <aguin005@ucr.edu>
2024-05-05 12:41:45 -0700

    - Added goal state to compare - Fixes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
         print(f"Default puzzle selected.")
         # Insert default puzzle here
     elif userChoice == '2':
-        print(f"Enter your puzzle, use a zero to represent the blank.")
+        print(f"Enter your puzzle, use a zero to represent the blank and press enter after each row.")
         firstRow = input("Enter the first row, use space or tabs between numbers: ")
         secondRow = input("Enter the second row, use space or tabs between numbers: ")
         thirdRow = input("Enter the third row, use space or tabs between numbers: ")
@@ -20,7 +20,7 @@ def main():
         userPuzzle = firstRow, secondRow, thirdRow
 
     
-    userAlgo = input("Enter your choice of algorithm: \nUniform Cost Search\nA* with the Misplaced Tile heuristic.\nA* with the Euclidean distance heuristic.\n")
+    userAlgo = input("Enter your choice of algorithm: \n1 for Uniform Cost Search\n2 for A* with the Misplaced Tile heuristic.\n3 for A* with the Euclidean distance heuristic.\nPress enter after choice of algorithm.\n")
 
     if userAlgo == 1:
         print(f"Uniform Cost Search selected.")
@@ -31,4 +31,19 @@ def main():
     elif userAlgo == 3:
         print(f"A* with the Euclidean distance heuristic selected.")
         # Do A* with Euclidean distance heuristic
+
+    # Checking Puzzle
+    print(userPuzzle)
+
+    if goal(userPuzzle):
+        print(f"Goal!")
+
+def goal(userPuzzle):
+    isGoal = False
+
+    if userPuzzle == (['1', '2', '3'], ['4', '5', '6'], ['7', '8', '*']) or (['1', '2', '3'], ['4', '5', '6'], ['7', '8', '0']):
+        isGoal = True
+
+    return isGoal
+
 main()
