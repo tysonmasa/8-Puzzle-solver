392171e424fad0d699f61088994443aa021e9aaa
Lackline <victor2965777@gmail.com>
2024-05-05 18:00:58 -0700

    defualt puzzle to fit main function

--- default.py
+++ default.py
@@ -1,8 +1,6 @@
-def print_puzzle(puzzle):
-    print("Very Easy")
-    for i in range(0, len(puzzle), 3):
-        row = puzzle[i:i+3]
-        print(' '.join(str(x) for x in row).replace('0', '*'))
-
-puzzle = [1, 2, 3, 4, 5, 6, 7, 0, 8]
-print_puzzle(puzzle)
+if user_choice == '1':
+        print(f"Default puzzle selected.")
+        # default puzzle, the very easy
+        default_puzzle = ['1', '2', '3', '4', '5', '6', '7', '0', '8'] 
+        # creat node
+        initial_node = Node(default_puzzle, 0, None, 0, None)
